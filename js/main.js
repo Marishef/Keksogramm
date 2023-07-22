5588a39043fa5165beb723500817d2bfedcca42c
Marina <marishef@bk.ru>
2023-07-22 10:34:52 +0300

    08.13.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 // import './data.js';
 import './util.js';
+import './download-foto';
 // import {getPhotosDescription} from './object-info-foto.js';
 import {PhotoDescriptions} from './data.js';
 import {createPicture} from './rendering-thumbnails.js';
