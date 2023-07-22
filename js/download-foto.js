e30fa4f84e1e3b5a5790576db369efa85bf7890d
Marina <marishef@bk.ru>
2023-07-22 16:49:28 +0300

    8.13.1.2

--- js/download-foto.js
+++ js/download-foto.js
@@ -3,12 +3,17 @@ const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const bodyModal = document.querySelector('body');
 const cancelButton = document.querySelector('#upload-cancel');
 
+const heshtegField = document.querySelector('.text__hashtags');
+const lineValid = /^#[A-Za-zА-Яа-яЁё0-9]{1-19}$/;
+
+//функция, вызываемая при нажатии esc
 function onEscKeyDown(evt) {
   if (evt.keyCode === 'Escape') {
         evt.preventDefault();
         closeModal();
   }
 }
+//открытие модального окна загрузки фото
 const showModal = () => {
   imgUploadOverlay.classList.remove('hidden');
   bodyModal.classList.add('modal-open');
@@ -16,10 +21,12 @@ const showModal = () => {
   document.removeEventListener('keydown', onEscKeyDown);
 }
 
+//событие: ожидание изменений при загрузки фото
 fileField.addEventListener('change' , () => {
   showModal();
 })
 
+// фцнкция закрытия окна
 const closeModal = () => {
   bimgUploadOverlay.classList.add('hidden');
   bodyModal.classList.remove('modal-open');
@@ -28,5 +35,16 @@ const closeModal = () => {
     showModal();
   });
 }
+
+//валидация формы
+const form = new Pristine (
+  form, {
+    ClassTo: 'img-upload__element',
+    errorTextParent: 'img-upload__element',
+    errorTextClass: 'img-upload__error',
+  }
+);
+
+
 
 
